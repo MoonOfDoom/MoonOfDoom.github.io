a523157139279bdc52cb55d109be9337831afcb9
MoidHasanBeig <MoidHasanBeig@github.com>
2020-11-02 21:45:21 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,128 @@
+// Our game scene
+
+var main_width = screen.width;
+var main_height = screen.height;
+
+var config = {
+	type: Phaser.AUTO,
+	width: main_width,
+	height: main_height,
+  scale: {
+      mode: Phaser.Scale.ENVELOP,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+	physics: {
+			default: 'arcade',
+			arcade: {
+					gravity: { y: 500 },
+					debug: false
+			}
+	},
+	scene: {
+			preload: preload,
+			create: create,
+			update: update
+	}
+};
+
+// Create the game with our config values
+// this will also inject our canvas element into the HTML source
+// for us
+var player;
+var platforms;
+var cursors;
+
+var game = new Phaser.Game(config);
+
+//scene functions
+// scene.init = function() {
+//
+// };
+
+function preload() {
+	this.load.image('sky', 'assets/sky.png');
+	this.load.image('ground', 'assets/platform.png');
+	// this.load.image('star', 'assets/star.png');
+	// this.load.image('bomb', 'assets/bomb.png');
+	this.load.spritesheet('dude',
+			'assets/dude.png',
+			{ frameWidth: 32, frameHeight: 48 }
+	);
+};
+
+function create() {
+	//background
+	this.add.image(main_width/2, main_height/2, 'sky');
+
+	//platforms
+	platforms = this.physics.add.staticGroup();
+
+	platforms.create(main_width/2, main_height, 'ground').setScale(4).refreshBody();
+
+	platforms.create(600, 250, 'ground');
+	platforms.create(50, 150, 'ground');
+	platforms.create(750, 100, 'ground');
+
+	//player
+	player = this.physics.add.sprite(100, 200, 'dude');
+
+	player.setBounce(0.1);
+	player.setCollideWorldBounds(true);
+
+	this.anims.create({
+	    key: 'left',
+	    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+	    frameRate: 10,
+	    repeat: -1
+	});
+
+	this.anims.create({
+	    key: 'turn',
+	    frames: [ { key: 'dude', frame: 4 } ],
+	    frameRate: 20
+	});
+
+	this.anims.create({
+	    key: 'right',
+	    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+	    frameRate: 10,
+	    repeat: -1
+	});
+
+	cursors = this.input.keyboard.createCursorKeys();
+
+	this.physics.add.collider(player, platforms);
+};
+
+function update() {
+
+	if (cursors.left.isDown)
+	{
+			player.setVelocityX(-100);
+
+			player.anims.play('left', true);
+	}
+	else if (cursors.right.isDown)
+	{
+			player.setVelocityX(100);
+
+			player.anims.play('right', true);
+	}
+	else
+	{
+			player.setVelocityX(0);
+
+			player.anims.play('turn');
+	}
+
+	if (cursors.up.isDown && player.body.touching.down)
+	{
+			player.setVelocityY(-330);
+	}
+
+};
+
+
+function end() {
+
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(3000, () => {
+  console.log("Server running @ 3000");
+});
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/index.html");
+})
